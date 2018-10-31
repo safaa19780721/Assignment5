beef2e48ae48a0f8477682c5edd0e2315b58cfe5
tylerhenniges <tylerhenniges@wireless83-011-149.wireless.wmich.edu>
2018-10-31 10:58:57 -0400

    Partner 2 change

--- Assignment5/src/Main.java
+++ Assignment5/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 			String[] filePaths = {"testcase1.txt","testcase2.txt", "testcase3.txt", "testcase4.txt", "testcase5.txt", "testcase6.txt", "testcase7.txt", "testcase8.txt", "testcase9.txt", "testcase10.txt"};
 			ContactInformationFormatter formatter = new ContactInformationFormatter();
 			formatter.readContactInformation(filePaths);
+			System.out.println("Partner 2 change");
 		}
 
 	}
