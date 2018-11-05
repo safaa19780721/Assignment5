d09894689fba9d94b13bb875ddf5f2ce90922566
ricky <ricky@10.80.141.13>
2018-11-05 13:58:10 -0500

    Ricky: Added shell of FormatExceptionHandler

--- Assignment5/src/FormatExceptionHandler.java
+++ Assignment5/src/FormatExceptionHandler.java
@@ -1,3 +1,29 @@
-public class FormatExceptionHandler {
+import java.io.*;
 
+public class FormatExceptionHandler implements IFormatExceptionHandler {
+
+	@Override
+	public void handleFileNotFoundException(FileNotFoundException e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handlePhoneNumberNotFoundException(PhoneNumberFormatException e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleEmailFormatNotFoundException(EmailAddressFormatException e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handleNameFormatNotFoundException(NameFormatException e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
