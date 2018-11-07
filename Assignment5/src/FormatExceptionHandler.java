4c0b025ceaa7be7a7012abbcd9d0a5d532793d3a
tylerhenniges <tylerhenniges@wireless83-009-135.wireless.wmich.edu>
2018-11-06 21:33:57 -0500

    Tyler: started work on FormatExceptionHandler

--- Assignment5/src/FormatExceptionHandler.java
+++ Assignment5/src/FormatExceptionHandler.java
@@ -4,14 +4,49 @@ public class FormatExceptionHandler implements IFormatExceptionHandler {
 
 	@Override
 	public void handleFileNotFoundException(FileNotFoundException e) {
-		// TODO Auto-generated method stub
-		
+		String fileName = e.getMessage(); //may need to add a message to this one to get filename
+		System.out.println(fileName + "The system cannot find the file specified");
 	}
 
 	@Override
 	public void handlePhoneNumberNotFoundException(PhoneNumberFormatException e) {
-		// TODO Auto-generated method stub
-		
+		String phoneNum = e.getMessage();
+		char[] phone = phoneNum.toCharArray();
+		char[] corrected = new char[14];
+		int i =0;
+		corrected[0] = '(';
+		int j = 1;
+		while(j < 4)
+		{
+			if(Character.isDigit(phone[i]))
+			{
+					corrected[j] = phone[i];
+					j++;
+			}
+			i++;
+		}
+		corrected[4] = ')';
+		corrected[5] = '-';
+		while(j < 9)
+		{
+			if(Character.isDigit(phone[i]))
+			{
+					corrected[j] = phone[i];
+					j++;
+			}
+			i++;
+		}
+		corrected[9] = '-';
+		while(j < 14)
+		{
+			if(Character.isDigit(phone[i]))
+			{
+					corrected[j] = phone[i];
+					j++;
+			}
+			i++;
+		}
+		System.out.println(corrected);
 	}
 
 	@Override
